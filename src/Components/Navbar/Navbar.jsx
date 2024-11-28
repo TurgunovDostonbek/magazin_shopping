14e634e07ef7bfee241c532259b28da444e4c4f4
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-11-28 15:22:38 +0500

    shopping

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,48 @@
 import "./Navbar.css";
-import { FaBars } from "react-icons/fa";
-import navLogo from "../../assets/img/Лого.png";
-import { CiSearch } from "react-icons/ci";
-import { CiUser } from "react-icons/ci";
+import navLike from "../../assets/img/like.png";
+import navBasket from "../../assets/img/basket.png";
+import navUser from "../../assets/img/user.png";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="container navbar_container">
-        <div className="navbar_logo flex_div">
-          <a href="#">
-            <img src={navLogo} alt="" />
-          </a>
+    <nav className="navbar">
+      <div className="navbar_container container">
+        <div className="navbar_katalog">
+          <button>Каталог</button>
         </div>
 
         <div className="navbar_search">
-          <form className="flex_div">
-            <label>
-              <input type="text" />
-            </label>
-            <button>
-              <CiSearch className="icons" />
-            </button>
-          </form>
+          <div className="navbar_search_input">
+            <form action="">
+              <input placeholder="Поиск товара" type="text" />
+            </form>
+          </div>
         </div>
-        <div className="navbar_icons">
-          <span>
-            <CiUser />
-            Войти
-          </span>
+
+        <div className="navbar_btn">
+          <div className="navbar_btn_like ">
+            <a href="#" className="flex_colum">
+              <img src={navLike} alt="Like" />
+              Избранное
+            </a>
+          </div>
+
+          <div className="navbar_btn_basket">
+            <a href="#" className="flex_colum">
+              <img src={navBasket} alt="Basket" />
+              Корзина
+            </a>
+          </div>
+
+          <div className="navbar_btn_user">
+            <a href="#" className="flex_colum">
+              <img src={navUser} alt="User" />
+              Войти
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
