28b85866dd5cf795b439cdfd493272757b9c1e82
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-12-14 14:24:09 +0500

    shopping 02

---
+++ src/data02.js
@@ -0,0 +1,447 @@
+// import Smartfon from "../src/assets/img/popularCategories/image 3.png";
+// import strilMashini from "../src/assets/img/popularCategories/image 4.png";
+// import plisos from "../src/assets/img/popularCategories/image 5.png";
+// import holodelnik from "../src/assets/img/popularCategories/image 6.png";
+// import televizor from "../src/assets/img/popularCategories/image 7.png";
+// import pech from "../src/assets/img/popularCategories/image 8.png";
+
+// export const popularCategories = [
+//   {
+//     id: 1,
+//     image: [Smartfon],
+//     title: "Смартфоны",
+//   },
+//   {
+//     id: 2,
+//     image: [strilMashini],
+//     title: "Стиральные машины",
+//   },
+//   {
+//     id: 3,
+//     image: [plisos],
+//     title: "Пылесосы",
+//   },
+//   {
+//     id: 4,
+//     image: [holodelnik],
+//     title: "Холодильники",
+//   },
+//   {
+//     id: 5,
+//     image: [televizor],
+//     title: "Телевизоры",
+//   },
+//   {
+//     id: 6,
+//     image: [pech],
+//     title: "Микроволновые печи",
+//   },
+// ];
+
+// export const Blogs = [
+//   {
+//     id: 1,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+//   {
+//     id: 2,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+//   {
+//     id: 3,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+//   {
+//     id: 4,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+// ];
+
+// //............................Service.........................//
+// // Service img
+// import servicesImg from "../src/assets/img/servis/servis img.png";
+
+// export const services = [
+//   {
+//     id: 1,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 2,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 3,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 4,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+// ];
+
+// //............................SeasonalGoods.........................//
+// // SeasonalGoods Img
+// import seasonalImg from "../src/assets/img/seasonal/Frame 1887.png";
+
+// export const productSeasonal = [
+//   {
+//     id: 1,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 2,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 3,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 4,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+// ];
+
+// //............................Bestseller.........................//
+// // Bestseller Img
+// import bestsellerImg from "../src/assets/img/bestseller/bestsellerimgggg.png";
+
+// export const bestSellers = [
+//   {
+//     id: 1,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 2,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 3,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 4,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+// ];
+
+// import Smartfon from "../src/assets/img/popularCategories/image 3.png";
+// import strilMashini from "../src/assets/img/popularCategories/image 4.png";
+// import plisos from "../src/assets/img/popularCategories/image 5.png";
+// import holodelnik from "../src/assets/img/popularCategories/image 6.png";
+// import televizor from "../src/assets/img/popularCategories/image 7.png";
+// import pech from "../src/assets/img/popularCategories/image 8.png";
+
+// export const popularCategories = [
+//   {
+//     id: 1,
+//     image: [Smartfon],
+//     title: "Смартфоны",
+//   },
+//   {
+//     id: 2,
+//     image: [strilMashini],
+//     title: "Стиральные машины",
+//   },
+//   {
+//     id: 3,
+//     image: [plisos],
+//     title: "Пылесосы",
+//   },
+//   {
+//     id: 4,
+//     image: [holodelnik],
+//     title: "Холодильники",
+//   },
+//   {
+//     id: 5,
+//     image: [televizor],
+//     title: "Телевизоры",
+//   },
+//   {
+//     id: 6,
+//     image: [pech],
+//     title: "Микроволновые печи",
+//   },
+// ];
+
+// export const Blogs = [
+//   {
+//     id: 1,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+//   {
+//     id: 2,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+//   {
+//     id: 3,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+//   {
+//     id: 4,
+//     image: "../src/assets/img/blogs/blogs.png",
+//     data: "21.03.24",
+//     title: "Самые популярные смартфоны 2024",
+//     info: "Специально для вас подготовили подборку самых популярных смартфонов.",
+//   },
+// ];
+
+// //............................Service.........................//
+// // Service img
+// import servicesImg from "../src/assets/img/servis/servis img.png";
+
+// export const services = [
+//   {
+//     id: 1,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 2,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 3,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 4,
+//     image: [servicesImg],
+//     name: "Настройка SMART TV",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+// ];
+
+// //............................SeasonalGoods.........................//
+// // SeasonalGoods Img
+// import seasonalImg from "../src/assets/img/seasonal/Frame 1887.png";
+
+// export const productSeasonal = [
+//   {
+//     id: 1,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 2,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 3,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 4,
+//     image: [seasonalImg],
+//     name: "Робот для мытья окон Даджет dBot W120",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+// ];
+
+// //............................Bestseller.........................//
+// // Bestseller Img
+// import bestsellerImg from "../src/assets/img/bestseller/bestsellerimgggg.png";
+
+// export const bestSellers = [
+//   {
+//     id: 1,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 2,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 3,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+//   {
+//     id: 4,
+//     image: [bestsellerImg],
+//     name: "Смартфон TECNO Pova 4 LG7n 8GB/128GB (серый)",
+//     rating: 5.0,
+//     discount: "60%",
+//     price: 1200,
+//     originalPrice: 3000,
+//     currency: "₽",
+//     isAdvertisement: false,
+//   },
+// ];
